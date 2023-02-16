51ea6baa9e837cbc309d8a75c5ebeb37b9bca3ba
debartan <debartan@gmail.com>
2023-02-16 14:40:18 -0500

    added loop and graps

--- baja_anal.py
+++ baja_anal.py
@@ -5,63 +5,51 @@ from scipy import signal
 import matplotlib.pyplot as plt
 import os
 
+comp_name = "Tennessee"
+
 main_dir = os.path.dirname(__file__)
-rel_path = "Data/Tennessee.csv"
+rel_path = "Data/"+comp_name+".csv"
 file_path = os.path.join(main_dir, rel_path)
 
-data = np.genfromtxt(file_path, delimiter=',', dtype=float, skip_header=1,
-        filling_values=0)
-
+# mashing data to fit form I want
+data = np.genfromtxt(file_path, delimiter=',',filling_values=0,dtype=None,
+        encoding=None)
+event_names = data[0]
+data = data[1:]
+data = data.astype('float64')
 data = data.T
 
-f_cost = signal.savgol_filter(data[4],50,4)
-cost = plt.subplot(3,3,1)
-cost.plot(data[4])
-cost.title.set_text("COST")
-cost.plot(f_cost)
 
-f_design = signal.savgol_filter(data[5],50,4)
-design = plt.subplot(3,3,2)
-design.plot(data[5])
-design.title.set_text("DESIGN")
-design.plot(f_design)
+car_number = 94
+f_window = 75
+f_order = 4
 
-f_sales = signal.savgol_filter(data[6],50,4)
-sales = plt.subplot(3,3,3)
-sales.plot(data[6])
-sales.title.set_text("SALES")
-sales.plot(f_sales)
+width = 4
+size = len(event_names)
+if size%width==0:
+    height = size/width
+else:
+    height = size//width+1
 
-f_accel = signal.savgol_filter(data[7],50,4)
-accel = plt.subplot(3,3,4)
-accel.plot(data[7])
-accel.title.set_text("accel")
-accel.plot(f_accel)
 
-f_man = signal.savgol_filter(data[8],50,4)
-man = plt.subplot(3,3,5)
-man.plot(data[8])
-man.title.set_text("MANEUV")
-man.plot(f_man)
+for index, event in enumerate(event_names):
+    # sorting and filtering data for event
 
-f_sled = signal.savgol_filter(data[9],50,4)
-sled = plt.subplot(3,3,6)
-sled.plot(data[9])
-sled.title.set_text("SLED")
-sled.plot(f_sled)
+    sort_y = np.sort(data[index])
+    sort_y = sort_y[::-1]
+    filt_y = signal.savgol_filter(sort_y,f_window,f_order)
 
-f_susp = signal.savgol_filter(data[10],50,4)
-susp = plt.subplot(3,3,7)
-susp.plot(data[10])
-susp.title.set_text("S&T")
-susp.plot(f_susp)
+    # finding position our car number was in event
+    sort_index = data[index].argsort()
+    cars_sorted = data[1,sort_index[::-1]]
+    event_pos = np.where(cars_sorted == car_number)
 
-f_end = signal.savgol_filter(data[11],50,4)
-end = plt.subplot(3,3,8)
-end.plot(data[11])
-end.title.set_text("ENDURANCE")
-end.plot(f_end)
-
+    # plotting
+    end = plt.subplot(width,height,index+1)
+    end.plot(sort_y)
+    end.plot(filt_y)
+    end.axvline(x=event_pos[0], color="red")
+    end.title.set_text(event)
 
 plt.show()
 
